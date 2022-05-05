9fea8f086f0fdcee0518b01cc33e741b965a41a8
David Wheatley <hi@davwheat.dev>
2022-05-05 14:28:33 +0100

    feat: create emails repository

---
+++ src/UserEmailRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Blomstra\PostByMail;
+
+use Flarum\User\User;
+use Illuminate\Database\Eloquent\Builder;
+use Blomstra\PostByMail\UserEmail;
+use Illuminate\Database\Eloquent\Collection;
+
+class UserEmailRepository
+{
+    /**
+     * @return Builder
+     */
+    public function query()
+    {
+        return UserEmail::query();
+    }
+
+    /**
+     * @param int $id
+     * @param User $actor
+     * @return UserEmail
+     */
+    public function findOrFail($id, User $actor = null): UserEmail
+    {
+        $email = UserEmail::findOrFail($id);
+
+        $actor->assertCan('viewAdditionalEmailAddresses', $email->user());
+
+        return $email;
+    }
+
+    public function findAllForUser(User $user, User $actor = null): Collection
+    {
+        $actor->assertCan('viewAdditionalEmailAddresses', $user);
+
+        return UserEmail::where('user_id', $actor->id)->get();
+    }
+}
